a0d692e28f04a7edc6c91f4153ec8563b8a1d25c
Scott Parry <451109+scottparry@users.noreply.github.com>
2024-05-15 07:32:21 +0100

    Update card-author-bio.php
    Change Author Bio card to display nothing if not set.

--- patterns/card-author-bio.php
+++ patterns/card-author-bio.php
@@ -12,10 +12,6 @@
  * Inserter: true
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|80","right":"var:preset|spacing|80"},"blockGap":"var:preset|spacing|40"},"border":{"radius":"5px"}},"backgroundColor":"light-grey","layout":{"type":"default"}} -->
-<div class="wp-block-group has-light-grey-background-color has-background" style="border-radius:5px;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--80)"><!-- wp:heading {"level":3,"className":"is-style-default","fontSize":"large-stretch"} -->
-<h3 class="wp-block-heading is-style-default has-large-stretch-font-size">About</h3>
-<!-- /wp:heading -->
-
-<!-- wp:post-author-biography {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}},"typography":{"lineHeight":1.6}},"fontSize":"medium-stretch"} /--></div>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"0"},"border":{"radius":"5px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="border-radius:5px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:post-author-biography {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}},"typography":{"lineHeight":1.6}},"backgroundColor":"light-grey","fontSize":"medium-stretch"} /--></div>
 <!-- /wp:group -->
