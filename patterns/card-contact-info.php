bece5a7ff660be7482b00d8e70e74b41c061a6eb
Scott Parry <451109+scottparry@users.noreply.github.com>
2024-05-16 17:24:17 +0100

    Create card-contact-info.php

---
+++ patterns/card-contact-info.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Title: Contact Info Card
+ * Slug: card-contact-info
+ * Description: A basic contact info card
+ * Categories: rebootwp/cards
+ * Keywords: about, contact, card, content, layout, template, block
+ * Viewport Width: 1360
+ * Template Types:
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70","right":"var:preset|spacing|70"}}},"backgroundColor":"primary-accent","textColor":"white","layout":{"type":"constrained","contentSize":"1360px"}} -->
+<div class="wp-block-group has-white-color has-primary-accent-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","className":"is-style-default","fontSize":"x-large"} -->
+<h2 class="wp-block-heading is-style-default has-white-color has-text-color has-link-color has-x-large-font-size">Get In Touch.</h2>
+<!-- /wp:heading -->
+
+<!-- wp:separator {"backgroundColor":"twentyfiveopaquewhite","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-twentyfiveopaquewhite-color has-alpha-channel-opacity has-twentyfiveopaquewhite-background-color has-background is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|fiftyopaquewhite"}}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"fiftyopaquewhite","fontSize":"x-small"} -->
+<p class="has-fiftyopaquewhite-color has-text-color has-link-color has-x-small-font-size" style="font-style:normal;font-weight:700">email:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"large"} -->
+<p class="has-large-font-size" style="font-style:normal;font-weight:500">get@rebootwp.com</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:separator {"backgroundColor":"twentyfiveopaquewhite","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-twentyfiveopaquewhite-color has-alpha-channel-opacity has-twentyfiveopaquewhite-background-color has-background is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|fiftyopaquewhite"}}}},"textColor":"fiftyopaquewhite","fontSize":"x-small"} -->
+<p class="has-fiftyopaquewhite-color has-text-color has-link-color has-x-small-font-size" style="font-style:normal;font-weight:700">telephone:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"large"} -->
+<p class="has-large-font-size" style="font-style:normal;font-weight:500">123-456-7890</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:separator {"backgroundColor":"twentyfiveopaquewhite","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-twentyfiveopaquewhite-color has-alpha-channel-opacity has-twentyfiveopaquewhite-background-color has-background is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|fiftyopaquewhite"}}}},"textColor":"fiftyopaquewhite","fontSize":"x-small"} -->
+<p class="has-fiftyopaquewhite-color has-text-color has-link-color has-x-small-font-size" style="font-style:normal;font-weight:700">address:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"},"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"default"},"fontSize":"large"} -->
+<div class="wp-block-group has-large-font-size" style="font-style:normal;font-weight:500"><!-- wp:paragraph -->
+<p>123, Reboot Avenue</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>London, UK</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>SW1</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:separator {"backgroundColor":"twentyfiveopaquewhite","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-twentyfiveopaquewhite-color has-alpha-channel-opacity has-twentyfiveopaquewhite-background-color has-background is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:social-links {"iconColor":"white","iconColorValue":"#ffffff","openInNewTab":true,"className":"is-style-logos-only"} -->
+<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group -->
