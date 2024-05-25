17aa17db9c98125e699f2df2708c12e8bb722726
Scott Parry <451109+scottparry@users.noreply.github.com>
2024-05-25 09:48:24 +0100

    Improve Footers

--- patterns/footer-dark.php
+++ patterns/footer-dark.php
@@ -20,12 +20,10 @@
 <!-- wp:group {"layout":{"type":"constrained","contentSize":"1360px"}} -->
 <div class="wp-block-group"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"width":"30%"} -->
-<div class="wp-block-column" style="flex-basis:30%"><!-- wp:image {"id":8,"width":"125px","height":"auto","sizeSlug":"large","linkDestination":"none"} -->
-<figure class="wp-block-image size-large is-resized"><img src="https://rebootwp.com/wp-content/uploads/2024/05/light-logo-2048x441.webp" alt="reboot wp light logo" class="wp-image-8" style="width:125px;height:auto"/></figure>
-<!-- /wp:image -->
+<div class="wp-block-column" style="flex-basis:30%"><!-- wp:site-title {"fontSize":"large"} /-->
 
 <!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.5"}},"fontSize":"medium"} -->
-<p class="has-medium-font-size" style="line-height:1.5">This is the future, a <strong>new era of WordPress</strong> themes. Start building websites faster &amp; easier than ever.</p>
+<p class="has-medium-font-size" style="line-height:1.5">Design without ever leaving your browser, customising every detail in real time — no coding required.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
@@ -39,8 +37,8 @@
 <!-- /wp:spacer -->
 
 <!-- wp:group {"layout":{"type":"default"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"500"},"elements":{"link":{"color":{"text":"var:preset|color|seventyfiveopaquewhite"}}}},"textColor":"seventyfiveopaquewhite","className":"reboot-text-gradient","fontSize":"xxx-small"} -->
-<p class="has-text-align-left reboot-text-gradient has-seventyfiveopaquewhite-color has-text-color has-link-color has-xxx-small-font-size" style="font-style:normal;font-weight:500;text-transform:uppercase">© 2023 Reboot · Powered by <a href="https://rebootwp.com/">RebootWP</a> &amp; <a href="https://wordpress.org/">WordPress</a>.</p>
+<div class="wp-block-group"><!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"500"},"elements":{"link":{"color":{"text":"var:preset|color|seventyfiveopaquewhite"}}}},"textColor":"seventyfiveopaquewhite","className":"reboot-text-gradient is-style-rebootwp-paragraph-gradient","fontSize":"xxx-small"} -->
+<p class="has-text-align-left reboot-text-gradient is-style-rebootwp-paragraph-gradient has-seventyfiveopaquewhite-color has-text-color has-link-color has-xxx-small-font-size" style="font-style:normal;font-weight:500;text-transform:uppercase">© 2023 · Powered by <a href="https://rebootwp.com/">RebootWP</a> &amp; <a href="https://wordpress.org/">WordPress</a>.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
