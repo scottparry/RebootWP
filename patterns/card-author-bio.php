9b175c84a29cc2287b1577e94b9c148aebe01374
Scott Parry <451109+scottparry@users.noreply.github.com>
2024-05-25 20:36:41 +0100

    More line-height fixes

--- patterns/card-author-bio.php
+++ patterns/card-author-bio.php
@@ -13,5 +13,5 @@
  */
 ?>
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"0"},"border":{"radius":"5px"}},"layout":{"type":"default"}} -->
-<div class="wp-block-group" style="border-radius:5px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:post-author-biography {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}},"typography":{"lineHeight":1.6}},"backgroundColor":"light-grey","fontSize":"medium-stretch"} /--></div>
+<div class="wp-block-group" style="border-radius:5px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:post-author-biography {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}},"backgroundColor":"light-grey","fontSize":"medium-stretch"} /--></div>
 <!-- /wp:group -->
