d61bcd92ca8a4b10cc6b611fee36833495bea2c2
Scott Parry <451109+scottparry@users.noreply.github.com>
2024-05-25 09:14:55 +0100

    Improve Headers

--- patterns/header-default.php
+++ patterns/header-default.php
@@ -14,7 +14,7 @@
 ?>
 <!-- wp:group {"tagName":"header","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"position":{"type":""}},"backgroundColor":"twentyfiveopaquewhite","textColor":"white","className":"is-style-rebootwp-column-box-shadow","layout":{"type":"constrained"}} -->
 <header class="wp-block-group is-style-rebootwp-column-box-shadow has-white-color has-twentyfiveopaquewhite-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide"><!-- wp:site-logo {"width":125} /-->
+<div class="wp-block-group alignwide"><!-- wp:site-title {"style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","fontSize":"large"} /-->
 
 <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
 <div class="wp-block-group"><!-- wp:navigation {"ref":4,"textColor":"secondary","icon":"menu","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"nowrap"},"style":{"spacing":{"blockGap":"var:preset|spacing|50"},"layout":{"selfStretch":"fit","flexSize":null},"typography":{"fontStyle":"normal","fontWeight":"600","textTransform":"uppercase"}},"fontSize":"x-small"} /--></div>
