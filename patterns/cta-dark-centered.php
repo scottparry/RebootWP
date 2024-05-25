e7e3a5fc7dd5f973463d92390cfd4ce0f57eb15e
Scott Parry <451109+scottparry@users.noreply.github.com>
2024-05-25 12:52:51 +0100

    Add CTA patterns

---
+++ patterns/cta-dark-centered.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Title: CTA dark centered
+ * Slug: cta-dark-centered
+ * Description: CTA dark centered
+ * Categories: rebootwp/cta
+ * Keywords: text, cta, call to action, layout, template, block
+ * Viewport Width: 1360
+ * Template Types:
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80","right":"var:preset|spacing|80"}}},"backgroundColor":"primary","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-primary-background-color has-background" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"-1px"}},"textColor":"white","fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-text-align-center has-white-color has-text-color has-link-color has-x-large-font-size" style="font-style:normal;font-weight:600;letter-spacing:-1px">Design without ever leaving your browser.</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|seventyfiveopaquewhite"}}},"typography":{"lineHeight":1.6,"letterSpacing":"-0.5px"}},"textColor":"seventyfiveopaquewhite","fontSize":"medium"} -->
+<p class="has-text-align-center has-seventyfiveopaquewhite-color has-text-color has-link-color has-medium-font-size" style="letter-spacing:-0.5px;line-height:1.6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"5px"} -->
+<div style="height:5px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","backgroundColor":"primary-accent","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+<div class="wp-block-button" style="font-style:normal;font-weight:600"><a class="wp-block-button__link has-primary-accent-background-color has-background has-text-align-center wp-element-button" href="#">Get Started</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"white"} -->
+<p class="has-white-color has-text-color has-link-color" style="font-style:normal;font-weight:600"><a href="#">Learn more →</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
