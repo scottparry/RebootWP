ca5023cff41bacb0b9c7cbe8fadf6e1b2a60fbc4
Scott Parry <451109+scottparry@users.noreply.github.com>
2024-05-29 12:49:03 +0100

    Update testimonial-single-block.php
    Remove constrained layout type for easier user editing

--- patterns/testimonial-single-block.php
+++ patterns/testimonial-single-block.php
@@ -12,7 +12,7 @@
  * Inserter: true
  */
 ?>
-<!-- wp:group {"style":{"border":{"radius":"5px"},"spacing":{"blockGap":"var:preset|spacing|60","padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80","right":"var:preset|spacing|80"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"primary-accent","textColor":"white","layout":{"type":"constrained"}} -->
+<!-- wp:group {"style":{"border":{"radius":"5px"},"spacing":{"blockGap":"var:preset|spacing|60","padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80","right":"var:preset|spacing|80"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"primary-accent","textColor":"white","layout":{"type":"default"}} -->
 <div class="wp-block-group has-white-color has-primary-accent-background-color has-text-color has-background has-link-color" style="border-radius:5px;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"xx-large"} -->
 <h2 class="wp-block-heading has-white-color has-text-color has-link-color has-xx-large-font-size">"Designing with Patterns and the WordPress Site Editor is so intuitive and super fast!"</h2>
 <!-- /wp:heading -->
@@ -21,7 +21,7 @@
 <hr class="wp-block-separator has-text-color has-twentyfiveopaquewhite-color has-alpha-channel-opacity has-twentyfiveopaquewhite-background-color has-background is-style-wide"/>
 <!-- /wp:separator -->
 
-<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"constrained"}} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"default"}} -->
 <div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"medium"} -->
 <p class="has-white-color has-text-color has-link-color has-medium-font-size" style="font-style:normal;font-weight:700">Jane Doe</p>
 <!-- /wp:paragraph -->
