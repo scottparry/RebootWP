ce19df58463134b87f08f4fa1ee373b514b7e88e
Scott Parry <451109+scottparry@users.noreply.github.com>
2024-05-30 13:15:34 +0100

    Add new header patterns with page title

---
+++ patterns/header-dark-page-title.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Title: Header Dark with Page Title
+ * Slug: header-dark-page-title
+ * Description:  Header Dark with Page Title
+ * Categories: rebootwp/headers
+ * Keywords: headers, layout, template, action, call-to
+ * Viewport Width: 1360
+ * Template Types:
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"right":"0","left":"0","top":"0","bottom":"0"}}},"backgroundColor":"primary","layout":{"type":"default"}} -->
+<div class="wp-block-group has-primary-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"0","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}}},"layout":{"type":"constrained","contentSize":"1360px"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--60);padding-bottom:0;padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:site-title {"level":2,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} /-->
+
+<!-- wp:navigation {"ref":4,"textColor":"seventyfiveopaquewhite"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"bottom":{"color":"var:preset|color|tenopaquewhite","width":"1px"},"top":[],"right":[],"left":[]}},"backgroundColor":"white","className":"is-style-rebootwp-group-box-shadow","layout":{"type":"default"}} -->
+<div class="wp-block-group is-style-rebootwp-group-box-shadow has-white-background-color has-background" style="border-bottom-color:var(--wp--preset--color--tenopaquewhite);border-bottom-width:1px"><!-- wp:group {"layout":{"type":"constrained","contentSize":"1360px"}} -->
+<div class="wp-block-group"><!-- wp:post-title {"level":3,"style":{"typography":{"letterSpacing":"-0.5px"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
