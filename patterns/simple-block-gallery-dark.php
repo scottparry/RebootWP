f03d76dddb2a0554b5e06275a1ba892b40a17fc3
Scott Parry <451109+scottparry@users.noreply.github.com>
2024-05-30 10:10:11 +0100

    Add new simple block pattern galleries

---
+++ patterns/simple-block-gallery-dark.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Title: Simple Block Gallery Dark
+ * Slug: simple-block-gallery-dark
+ * Description: Simple Block Gallery Dark
+ * Categories: rebootwp/gallery, rebootwp/portfolio
+ * Keywords: portfolio, gallery, images, photos, content, layout, template, block
+ * Viewport Width: 1360
+ * Template Types:
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}}},"backgroundColor":"primary","layout":{"type":"default"}} -->
+<div class="wp-block-group has-primary-background-color has-background" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"layout":{"type":"constrained","contentSize":"960px"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-text-align-center has-white-color has-text-color has-link-color has-x-large-font-size">Checkout some of our recent work</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|seventyfiveopaquewhite"}}}},"textColor":"seventyfiveopaquewhite","fontSize":"large"} -->
+<p class="has-text-align-center has-seventyfiveopaquewhite-color has-text-color has-link-color has-large-font-size">Anim sit consequat culpa commodo eu do nisi commodo ut aute aliqua. Laborum esse duis tempor consectetur officia mollit fugiat.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"15px"} -->
+<div style="height:15px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"layout":{"type":"constrained","contentSize":"1360px"}} -->
+<div class="wp-block-group"><!-- wp:gallery {"columns":3,"linkTo":"none"} -->
+<figure class="wp-block-gallery has-nested-images columns-3 is-cropped"><!-- wp:image {"lightbox":{"enabled":true},"id":427,"sizeSlug":"large","linkDestination":"none","style":{"color":[]}} -->
+<figure class="wp-block-image size-large"><img src="https://rebootwp.com/assets/images/placeholder.webp" alt="" class="wp-image-427"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"lightbox":{"enabled":true},"id":412,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://rebootwp.com/assets/images/placeholder.webp" alt="" class="wp-image-412"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"lightbox":{"enabled":true},"id":288,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://rebootwp.com/assets/images/placeholder.webp" alt="" class="wp-image-288"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"lightbox":{"enabled":true},"sizeSlug":"large","linkDestination":"none","style":{"color":[]}} -->
+<figure class="wp-block-image size-large"><img src="https://rebootwp.com/assets/images/placeholder.webp" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"lightbox":{"enabled":true},"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://rebootwp.com/assets/images/placeholder.webp" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"lightbox":{"enabled":true},"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://rebootwp.com/assets/images/placeholder.webp" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"lightbox":{"enabled":true},"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://rebootwp.com/assets/images/placeholder.webp" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"lightbox":{"enabled":true},"sizeSlug":"large","linkDestination":"none","style":{"color":[]}} -->
+<figure class="wp-block-image size-large"><img src="https://rebootwp.com/assets/images/placeholder.webp" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"lightbox":{"enabled":true},"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://rebootwp.com/assets/images/placeholder.webp" alt=""/></figure>
+<!-- /wp:image --></figure>
+<!-- /wp:gallery --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
