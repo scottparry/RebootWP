4f40d1746ca891a11c2a6d75592eb830809e29f3
Scott Parry <451109+scottparry@users.noreply.github.com>
2024-06-04 16:56:34 +0100

    Add banner patterns

---
+++ patterns/banner-brand-colour-centered.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Banner Brand Colour Centered
+ * Slug: banner-brand-colour-centered
+ * Description: Banner Brand Colour Centered
+ * Categories: rebootwp/banners
+ * Keywords: cta, banner, layout, template, block
+ * Viewport Width: 1360
+ * Template Types:
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"}},"backgroundColor":"primary-accent","layout":{"type":"default"}} -->
+<div class="wp-block-group has-primary-accent-background-color has-background"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"x-small"} -->
+<p class="has-text-align-center has-white-color has-text-color has-link-color has-x-small-font-size"><strong>RebootWP Pro</strong> Coming Soon.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"typography":{"textDecoration":"underline"},"layout":{"selfStretch":"fit","flexSize":null}},"textColor":"white","fontSize":"x-small"} -->
+<p class="has-white-color has-text-color has-link-color has-x-small-font-size" style="text-decoration:underline"><a href="#">Subscribe to be notified →</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/banner-light-button.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: Banner Light with Button
+ * Slug: banner-light-button
+ * Description: Banner Light with Button
+ * Categories: rebootwp/banners
+ * Keywords: cta, banner, layout, template, block
+ * Viewport Width: 1360
+ * Template Types:
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"}},"backgroundColor":"white","layout":{"type":"default"}} -->
+<div class="wp-block-group has-white-background-color has-background"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","fontSize":"x-small"} -->
+<p class="has-text-align-center has-primary-color has-text-color has-link-color has-x-small-font-size"><strong>RebootWP Pro</strong> Coming Soon. Subscribe to be notified.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"primary-accent","style":{"spacing":{"padding":{"left":"var:preset|spacing|40","right":"var:preset|spacing|40","top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}},"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"x-small"} -->
+<div class="wp-block-button has-custom-font-size has-x-small-font-size" style="font-style:normal;font-weight:500"><a class="wp-block-button__link has-primary-accent-background-color has-background wp-element-button" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--40)">Subscribe</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
